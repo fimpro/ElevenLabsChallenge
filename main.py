b31153e65b9b60b08abd85b34c81a8ac28b4ba23
fimpro <118761502+fimpro@users.noreply.github.com>
2024-10-01 12:15:45 +0200

    Changing app core

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+import asyncio
+import websockets
 
-app = FastAPI()
+def location_to_poi():
+    pass
 
+def poi_to_audio():
+    pass
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def handle_connection(websocket, path):
+    print(websocket.remote_address)
+    async for message in websocket:
+        print(f"Received message: {message}")
+        response = f"Hello from Python, you said: {message, path}"
+        await websocket.send(response)
 
+async def main():
+    async with websockets.serve(handle_connection, "localhost", 8765):
+        print("WebSocket server running on ws://localhost:8765")
+        await asyncio.Future()  # keep the server running
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == '__main__':
+    asyncio.run(main())
