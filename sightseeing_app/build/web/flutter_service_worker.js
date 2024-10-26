8d1f040bd12d0fe9fe587eb9a981ca0a6bb37dc3
Jakub L <jakub@cubepotato.eu>
2024-10-26 23:13:37 +0200

    test

--- sightseeing_app/build/web/flutter_service_worker.js
+++ sightseeing_app/build/web/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "7d1e139ba45f6c8f2e37f3f23661303a
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "528b19c453c8258cf0febdeca0c2e7e8",
+"flutter_bootstrap.js": "f0853da4b43b2f9b72753140b49ad8e0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "367a380d76618ee5a2bd9231774b7461",
 "/": "367a380d76618ee5a2bd9231774b7461",
-"main.dart.js": "af98e5aff67fc568a28c8b27a8757e5e",
+"main.dart.js": "0f39882a64e4318ff8091e1900066063",
 "manifest.json": "a45f68c6d31501c4e42f187a2f15cad4",
 "version.json": "86985d6c6a37c64b7eff9394068f2e4b"};
 // The application shell files that are downloaded before a service worker can
